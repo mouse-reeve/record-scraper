fed3430f2a32d3df8028a31e366857bbf1a3e1ea
Mouse Reeve <mouse.reeve@gmail.com>
2015-08-10 17:49:42 -0700

    Adds basic stats

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # record-scraper
 
 Metadata about my [record collection](http://www.discogs.com/user/tripofmice/collection) crawled from Discogs
+
+Top artists:
+-5 "Sleater-Kinney"
+-3 "Rudimentary Peni"
+-2 "White Lung"
+
+Top styles:
+-18 "Punk"
+-5 "Indie Rock"
+-4 "Alternative Rock"
+
---
+++ generateReadme.sh
@@ -0,0 +1,15 @@
+echo "# record-scraper
+
+Metadata about my [record collection](http://www.discogs.com/user/tripofmice/collection) crawled from Discogs
+
+Top artists:
+$( jq .[].artist items.json | sort | uniq -c | sort -r | head -3 | while read line; do
+    echo "-$line"
+done )
+
+Top styles:
+$( jq .[].style items.json | sort | uniq -c | sort -r | head -3 | while read line; do
+    echo "-$line"
+done )
+" > README.md
+
